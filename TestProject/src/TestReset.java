69c92a3f047c2dc0e5a847d693df85f08100e487
priyanka4 <priyanka4@hpe.com>
2018-02-25 19:51:51 +0530

    resolved conflict in testreset for rebase

--- TestProject/src/TestReset.java
+++ TestProject/src/TestReset.java
@@ -6,7 +6,7 @@ public class TestReset {
 		System.out.println("Test reset");
 		System.out.println("Test reset new after stash");
 		System.out.println("Test reset new after  new commit");
-		
+	Show stash example
 
 	}
 
