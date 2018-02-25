01c5a7397bf3f1fe26bcf7c0960e7c9db32d8dbf
priyanka4 <priyanka4@hpe.com>
2018-02-25 23:55:04 +0530

    Squashed commit of the following:
    commit 22db0811392e6298b28c587ca4bc8436eb35e6a0
    Author: priyanka4 <priyanka4@hpe.com>
    Date:   Sun Feb 25 23:43:18 2018 +0530
    
        Test Rebase changes

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 New Commit in Test branch1
 
 New Commit in Master branch1
-
 Commit 1.1 in master branch
-
 Commit 1.2 in master branch
 
 Commit 2.1 in master branch
 
 Commit 2.2 in master branch
+
+Commit 1.1 in TestRebase branch
--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -4,6 +4,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		//ADDing comment 1 in testRebase branch
+		//Adding commit2 in testRebase branch
 
 	}
 
