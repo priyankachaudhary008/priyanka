63f09f28e53c41743f37f720493f26978798a3d5
priyanka4 <priyanka4@hpe.com>
2018-03-02 18:28:36 +0530

    Final Commit

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -7,8 +7,12 @@ public class Test {
 		//ADDing comment 1 in testRebase branch
 		//Adding commit2 in testRebase branch
 		
-		
+		//Add Commit 1.3 in testRebase branch
 		//Add commit 1.1 in master branch
+		
+		//Add commit 1.2 in TestRebase branch
+		
+		//Add Commit 1.3 in testRebase branch
 
 	}
 
